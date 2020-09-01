6a17f8c6551df948d785198ae109998d4971d0f3
Valentina Cabrera <valentina.cabreram@correo.ucu.edu.uy>
2020-09-01 20:25:18 -0300

    comment

--- Ejercicio 2/src/Program/Program.cs
+++ Ejercicio 2/src/Program/Program.cs
@@ -7,7 +7,7 @@ namespace SRP
     {
         static void Main(string[] args)
         {
-            Libro libro1 = new Libro("Design Patterns","Erich Gamma & Others","001-034");
+            Libro libro1 = new Libro("Design Patterns","Erich Gamma & Others","001-034"); //Cumple con el sistema SRP
             Libro libro2 = new Libro("Pro C#","Troelsen","001-035");
             libro1.AlmacenarLibro("A","7");
             libro2.AlmacenarLibro("B","3");
